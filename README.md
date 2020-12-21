3c27e6df8f58a223400e02d012dae8f7ba1d1624
neocaleb <neocaleb@gmail.com>
2020-12-21 18:20:16 +0100

    version1

---
+++ README.md
@@ -0,0 +1,14 @@
+# csv_to_10Xdata
+
+csv_to_10Xdata is a tool for transforming a gene expression matrix csv file into 10X style files.
+
+## Usage
+
+python csv_to_10Xdata.py matrix.csv
+
+## Output
+
+(1) matrix.mtx - expression file with MatrixMarket File Format
+(2) genes.tsv
+(3) barcodes.tsv
+
---
+++ csv_to_10Xdata.py
@@ -0,0 +1,30 @@
+import sys
+
+ifile = open(sys.argv[1])
+line = ifile.readline()
+gene_name=line.replace("\n","").split(",")[1:]
+
+ofile = open("gene.tsv","w")
+for i in range(len(gene_name)):
+    ofile.write(gene_name[i]+"\n")
+ofile.close()
+
+ofile = open("matrix.mtx","w")
+ofile.write("%%MatrixMarket matrix coordinate integer general\n")
+barcodes=[];colNum=1;dataNum=0
+for line in ifile:
+    temp=line.replace("\n","").split(",")
+    barcodes.append(temp[0])
+    for i in range(len(temp)-1):
+        if int(temp[i+1])>0:
+            ofile.write(str(i+1)+' '+str(colNum)+' '+temp[i+1]+'\n')
+            dataNum=dataNum+1
+    colNum=colNum+1
+ofile.seek(1)
+ofile.write(str(len(gene_name))+" "+str(len(barcodes))+" "+str(dataNum)+'\n')
+ifile.close()
+
+ofile = open("barcodes.tsv","w")
+for i in range(len(barcodes)):
+    ofile.write(barcodes[i]+"\n")
+ofile.close()
